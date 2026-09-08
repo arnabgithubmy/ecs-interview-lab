3532f5d5987e29f6c9e33022ac2d18d75cf43817
arnabgithubmy <arnab.mondal@gmail.com>
2026-09-08 11:59:13 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "arnab-ecs-interview-terraform-state-730335474585"
+    bucket       = "arnab-ecs-interview-terraform-state-108383429107"
     key          = "ecs-interview/terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
