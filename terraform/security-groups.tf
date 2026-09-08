d23dbe7046e3432bc1cc66446f4fe2df832ac3ab
arnabgithubmy <arnab.mondal@gmail.com>
2026-09-08 12:28:10 +0530

    Update security-groups.tf

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -64,10 +64,10 @@ resource "aws_vpc_security_group_egress_rule" "ecs_outbound" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ecs_to_endpint" {
-  security_group_id = aws_security_group.endpoints.id
+  security_group_id            = aws_security_group.endpoints.id
   referenced_security_group_id = aws_security_group.ecs.id
-  from_port   = 443
-  to_port     = 443
-  ip_protocol = "tcp"
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "tcp"
 }
 
