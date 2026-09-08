8c7160cd8736902677c82818de3958e9c7c382eb
arnabgithubmy <arnab.mondal@gmail.com>
2026-09-08 12:27:00 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -17,17 +17,17 @@ data "aws_iam_policy_document" "ecs_task_assume_role" {
 }
 
 resource "aws_iam_role" "ecs_execution" {
-  name = "${var.project_name}-ecs-execution-role"
-  assume_role_policy =     data.aws_iam_policy_document.ecs_task_assume_role.json
+  name               = "${var.project_name}-ecs-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_execution" {
-  role = aws_iam_role.ecs_execution.name
+  role       = aws_iam_role.ecs_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_iam_role" "ecs_task" {
-  name = "${var.project_name}-ecs-task-role"
+  name               = "${var.project_name}-ecs-task-role"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
 }
 
@@ -50,7 +50,7 @@ data "aws_iam_policy_document" "dynamodb_access" {
 }
 
 resource "aws_iam_policy" "dynamodb_access" {
-  name = "${var.project_name}-dynamodb-access"
+  name   = "${var.project_name}-dynamodb-access"
   policy = data.aws_iam_policy_document.dynamodb_access.json
 }
 
